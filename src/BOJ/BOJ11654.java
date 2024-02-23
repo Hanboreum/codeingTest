e2e729aa38c69e1e80020a768542d1629ad79cb1
fullm <matoma1_@naver.com>
2024-02-24 02:36:07 +0900

    20240224 백준 11654

---
+++ src/BOJ/BOJ11654.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char ch = str.charAt(0);
+        //string 받아서 char 값으로 바꾸기
+        System.out.println((int)ch);
+        //int로 형변환해준다.
+    }
+}
