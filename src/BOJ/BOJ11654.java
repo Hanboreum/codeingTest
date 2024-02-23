d726a0694887379ccb8e7a97929eb89fd9ffbef9
fullm <matoma1_@naver.com>
2024-02-24 02:47:14 +0900

    BOJ10926

--- src/BOJ/BOJ11654.java
+++ src/BOJ/BOJ11654.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class BOJ11654 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String str= sc.next();
         char ch = str.charAt(0);
-        //string 받아서 char 값으로 바꾸기
         System.out.println((int)ch);
-        //int로 형변환해준다.
+
+        //숫자 -> 문자
+//        int num = sc.nextInt();
+//        System.out.println((char)num);
     }
 }
