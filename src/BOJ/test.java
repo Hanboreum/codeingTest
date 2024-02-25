fed748f8746c8fde6ddc09d81821e79b852a8928
fullm <matoma1_@naver.com>
2024-02-25 18:57:08 +0900

    20240225_BOJ2475
    조건문

--- src/BOJ/test.java
+++ src/BOJ/test.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int[] std = new  int[31];
+     Scanner sc = new Scanner(System.in);
+     int[] std = new int[31];
 
-       for(int i=0; i<28; i++){
-           int num =sc.nextInt();
-           std[num] =1;
-       }
-       for(int i=1; i<31; i++){
-           if(std[i]==0)
-               System.out.println(i);
-       }
+     for(int i=0; i<28; i++){
+         int num = sc.nextInt();
+         std[num] =1;
+     }
+     for(int i=1; i<30; i++){
+         if(std[i]==0)
+             System.out.println(i);
+     }
+
     }
 }
