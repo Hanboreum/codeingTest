472c92032c4a2c0ce9cab083ca4eaa24ce99ea6d
fullm <matoma1_@naver.com>
2024-02-25 16:47:12 +0900

    20240225_BOJ3003 값 초기화 후 입력 값 빼주기

---
+++ src/BOJ/BOJ3003.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ3003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      /*  int k = sc.nextInt();
+        int q = sc.nextInt();
+        int r = sc.nextInt();
+        int b = sc.nextInt();
+        int n = sc.nextInt();
+        //int num = sc.nextInt();
+        int[] arr =new int[num]; */
+        while (sc.hasNextLine()){
+            String num = sc.nextLine();
+            System.out.println(num);
+         }
+
+    }
+}
+
