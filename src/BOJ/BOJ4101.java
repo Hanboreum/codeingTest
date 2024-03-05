04bca5abbbd1f9dde248ed1bd1e2e1591be10fb1
fullm <matoma1_@naver.com>
2024-03-05 21:25:26 +0900

    20240305
    boj4101 정수 비교

---
+++ src/BOJ/BOJ4101.java
@@ -0,0 +1,23 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ4101 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while(true){
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+
+            if( a == 0 & b ==0)
+                break;
+            if( a > b){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+
+        }
+    }
+}
