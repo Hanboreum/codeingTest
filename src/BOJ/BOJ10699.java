b99a8b2a2d554b2c075ee149335c4d1a699b4e28
fullm <matoma1_@naver.com>
2024-03-05 21:08:05 +0900

    20240305
    boj10699 localdate.now();

---
+++ src/BOJ/BOJ10699.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class BOJ10699 {
+    public static void main(String[] args) {
+        LocalDate time = LocalDate.now();
+        System.out.println(time);
+    }
+}
