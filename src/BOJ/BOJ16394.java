433336f2163cfe782780ff45bf24146b44e82306
fullm <matoma1_@naver.com>
2024-03-07 23:47:16 +0900

    20240307
    숫자 역순 출력

---
+++ src/BOJ/BOJ16394.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        System.out.println(year - 1946);
+    }
+}
---
+++ src/BOJ/BOJ2742.java
@@ -0,0 +1,16 @@
+package BOJ;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for(int i=a; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
