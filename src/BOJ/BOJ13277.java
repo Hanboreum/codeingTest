719d4ef93d63c0e9e819e5c801469c70effc78b7
fullm <matoma1_@naver.com>
2024-03-09 14:07:26 +0900

    20240309

---
+++ src/BOJ/BOJ13277.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ13277 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        BigInteger x = sc.nextBigInteger();
+
+        System.out.println(n.multiply(x));
+
+    }
+}
