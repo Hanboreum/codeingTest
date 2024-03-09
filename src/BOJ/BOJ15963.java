fdce07d5265de0107352005d6eae8541c6d8a5b8
fullm <matoma1_@naver.com>
2024-03-09 14:16:28 +0900

    20240309
    long N = sc.nextLong();

---
+++ src/BOJ/BOJ15963.java
@@ -0,0 +1,17 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ15963 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       long N = sc.nextLong();
+       long M = sc.nextLong();
+
+       if( N == M){
+           System.out.println("1");
+       }else{
+           System.out.println("0");
+       }
+    }
+}
