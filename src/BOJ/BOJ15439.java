efe0c09fb895007467b5b935f1e3840bf2812022
fullm <matoma1_@naver.com>
2024-03-09 14:07:21 +0900

    20240309

---
+++ src/BOJ/BOJ15439.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ15439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N * N);
+    }
+}
--- src/BOJ/BOJ9086.java
+++ src/BOJ/BOJ9086.java
@@ -1,16 +1,16 @@
 package BOJ;
 
-import javax.sound.midi.SysexMessage;
 import java.util.Scanner;
 
 public class BOJ9086 {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int t = sc.nextInt();
-        for(int i=0; i<t; i++){
-         String text = sc.nextLine();
-         String answer = text.substring(0,1)+text.substring(text.length());
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       String[] str = new String[n];
 
-        }
+       for(int i=0; i< str.length; i++){
+
+       }
+
     }
 }
