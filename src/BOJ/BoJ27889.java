ccc35c0a2899f28287d7d69b02a356320c62e5b1
fullm <matoma1_@naver.com>
2024-03-10 15:04:56 +0900

    20240309

--- src/BOJ/BoJ27889.java
+++ src/BOJ/BoJ27889.java
@@ -9,14 +9,15 @@ public class BoJ27889 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
 
-        if(str.equals("NLCS")){
+        if (str.equals("NLCS")) {
             System.out.println("North London Collegiate School");
-        } else if(str.equals("BHA")){
+        } else if (str.equals("BHA")) {
             System.out.println("Branksome Hall Asia");
-        } else if(str.equals("KIS")){
+        } else if (str.equals("KIS")) {
             System.out.println("Korea International School");
-        } else if(str.equals("SJA")){
+        } else if (str.equals("SJA")) {
             System.out.println("St. Johnsbury Academy");
         }
+
     }
 }
